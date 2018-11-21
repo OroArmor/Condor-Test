960aec8471588e6423c7633529260c73f547a74b
s-oronae <s-oronae@52S500041226.it.bsd405.org>
2018-11-21 11:31:54 -0800

    update on new computer

--- src/org/usfirst/frc/team2412/robot/subsystems/CatapultSubsystem.java
+++ src/org/usfirst/frc/team2412/robot/subsystems/CatapultSubsystem.java
@@ -3,7 +3,9 @@ package org.usfirst.frc.team2412.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class CatapultSubsystem extends Subsystem {
-
+	
+	//flingy fun thingy
+	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
